24aced739d2e54bb2964304f173dbb4b3ea5b154
asliyalcin <70018246+asliyalcin@users.noreply.github.com>
2023-05-22 21:49:17 +0300

    Add files via upload

---
+++ deneme.py
@@ -0,0 +1,43 @@
+"""
+r=input("yaricap:")
+rr=float(r)
+p=3.14
+alan=p*(rr**2)
+cevre=2*p*rr
+print(alan)
+print(cevre)
+"""
+"""km=(input("aracin km bilgisi:"))
+mil=float(km)/1.6099344
+print(str(km)+"km="+str(mil)+" mil")"""
+
+"""name,surname,age,job="sadık","turan ",37," ögretmen"
+sonuc="benim adım "+name+" soyadım "+surname+str(age)+" yasında bir"+job+"im."
+sonuc=(f"benim adım {name} soyadım {surname} {age} yasındayım {job}im")
+print(sonuc)"""
+
+"""s=" hello world "
+sonuc=s.strip()
+print(sonuc)"""
+
+"""s="www.sadıkturan.com"
+sonuc=s.strip("w.com")
+print(sonuc)"""
+
+"""sonuc=s.count("a")
+print(sonuc)"""
+
+telefonlar=["samsung s5","samsung s6","samsung s7","samsung s8"]
+"""print(len(samsung))
+print(samsung[0])
+print(samsung[-1])
+samsung[0]="samsung s9"
+print(samsung[-3])
+if "samsung s6" in samsung:
+     print ("s6 listede var")"""
+
+ 
+ print(telefonlar[::-1] )
+
+
+      
